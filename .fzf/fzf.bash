9ffabf2a8a7f8c58c35c26c5636bc5e408c21dc0
Faithfulness Alamu <vaguemail369@gmail.com>
2021-04-24 12:10:06 +0100

    Add fzf configs

---
+++ .fzf/fzf.bash
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/thealamu/.fzf/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/thealamu/.fzf/key-bindings.bash"
